aa4195f20955b90bf8da274602ff97f244608fd0
Liu Xiaoyi <circuitcoder0@gmail.com>
2025-02-02 07:16:17 +0800

    Partial startup animation

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,22 +1,65 @@
-import {
-  init,
-  classModule,
-  propsModule,
-  styleModule,
-  eventListenersModule,
-  jsx
-} from "snabbdom";
+import * as CONFIG from "./config";
 
-const patch = init([
-  classModule,
-  propsModule,
-  styleModule,
-  eventListenersModule
-]);
+import { apply as applyStatic } from "./static";
+
+import { Post } from "./typings/Post";
+import dataUnty from "../../data.json";
+const data = dataUnty as Post[];
+
+/**
+ * Application bootstrap
+ */
+type State = {
+  ty: 'Vacant',
+} | {
+  ty: 'Home',
+} | {
+  ty: 'Post',
+  slug: string,
+} | {
+  ty: 'About',
+} | {
+  ty: 'NotFound',
+};
+
+let state: State = { ty: 'Vacant' };
 
 function bootstrap() {
-  const hello = <h1>Hello World</h1>;
-  patch(document.getElementById("app")!, hello);
+  applyStatic();
+
+  // TODO(ssr): Recover current state
+
+  // Bootstrap init animation
+  reflection(document.location.pathname);
+}
+
+/**
+ * Animation
+ */
+function transition(oldState: State, newState: State) {
+  // Do nothing now
+}
+
+/**
+ * URL -> state reflection
+ */
+function parsePath(path: String): State {
+  const postMatch = path.match(/^\/post\/([^\/]+)$/);
+  if (path === '/')
+    return { ty: 'Home' };
+  else if (path === '/about')
+    return { ty: 'About' };
+  else if(postMatch !== null)
+    return { ty: 'Post', slug: postMatch[1] };
+  else
+    return { ty: 'NotFound' };
+}
+
+function reflection(path: String) {
+  // Commit exit animation
+  const newState = parsePath(path);
+  transition(state, newState);
+  state = newState;
 }
 
 document.addEventListener('DOMContentLoaded', bootstrap);
---
+++ web/src/static.tsx
@@ -0,0 +1,67 @@
+/**
+ * Pure static HTML contents
+ */
+
+import {
+  init,
+  classModule,
+  propsModule,
+  attributesModule,
+  styleModule,
+  eventListenersModule,
+  jsx
+} from "snabbdom";
+
+const patch = init([
+  classModule,
+  propsModule,
+  attributesModule,
+  styleModule,
+  eventListenersModule
+]);
+
+const SQRT3 = Math.sqrt(3);
+const CORNER_RADIUS = 10;
+const logo = <svg attrs={{id: "logo", viewbox: "0 0 200 100"}}>
+  <defs>
+    <path attrs={{
+      id: "logo-shard",
+      d: `M -${CORNER_RADIUS/2*SQRT3} ${-50 + CORNER_RADIUS / 2 * 3}
+      A ${CORNER_RADIUS} ${CORNER_RADIUS} 0 0 1 ${CORNER_RADIUS / 2 * SQRT3} ${-50 + CORNER_RADIUS / 2 * 3}
+      L ${50 / SQRT3 - CORNER_RADIUS / 2 / SQRT3} -${CORNER_RADIUS / 2}
+      A ${CORNER_RADIUS} ${CORNER_RADIUS} 0 0 1 ${50 / SQRT3 - CORNER_RADIUS / 2 / SQRT3} ${CORNER_RADIUS / 2}
+      L ${CORNER_RADIUS/2*SQRT3} ${50 - CORNER_RADIUS / 2 * 3}
+      A ${CORNER_RADIUS} ${CORNER_RADIUS} 0 0 1 -${CORNER_RADIUS / 2 * SQRT3} ${50 - CORNER_RADIUS / 2 * 3}
+      L -${50 / SQRT3 - CORNER_RADIUS / 2 / SQRT3} ${CORNER_RADIUS / 2}
+      A ${CORNER_RADIUS} ${CORNER_RADIUS} 0 0 1 -${50 / SQRT3 - CORNER_RADIUS / 2 / SQRT3} -${CORNER_RADIUS / 2}
+      c
+    `
+    }} style={{
+      transform: `translate(50px, 50px)`
+    }}></path>
+
+    <mask attrs={{id: "logo-mask"}}>
+      <g>
+        <rect attrs={{ x: "50", y: "0", width: "150", height: "100", fill: "oklab(1 0 0)" }}></rect>
+        <use attrs={{ href: "#logo-shard" }} style={{
+          fill: "oklab(0 0 0)",
+        }}></use>
+      </g>
+    </mask>
+  </defs>
+  <g attrs={{id: "logo-shards"}}>
+    <g class={{ "logo-shards-container": true }}>
+      <use attrs={{ href: "#logo-shard", mask: "url(#logo-mask)" }}></use>
+    </g>
+    <g class={{ "logo-shards-container": true }}>
+      <use attrs={{ href: "#logo-shard", mask: "url(#logo-mask)" }}></use>
+    </g>
+    <g class={{ "logo-shards-container": true }}>
+      <use attrs={{ href: "#logo-shard" }}></use>
+    </g>
+  </g>
+</svg>
+
+export function apply() {
+  patch(document.getElementById("logo")!, logo);
+}
