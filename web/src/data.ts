20dede49e8e7fdbc9823fee71dd202bedfd8699b
Liu Xiaoyi <circuitcoder0@gmail.com>
2025-02-09 05:58:45 +0800

    Optimistic data preload + preload tag

--- web/src/data.ts
+++ web/src/data.ts
@@ -4,17 +4,18 @@ const SSR = import.meta.env.SSR;
 
 // TODO: regular cache invalidation
 // TODO: optimistic loading
-let cached: Post[] | null = null;
-export async function getData(): Promise<Post[]> {
+export async function getDataInner(): Promise<Post[]> {
   // @ts-ignore
   if(SSR) {
     const fs = await import('node:fs/promises');
     return JSON.parse((await fs.readFile('./dist/client/data.json')).toString('utf-8')) as Post[];
   }
 
-  if(cached === null) {
-    const req = await fetch("/data.json");
-    cached = await req.json();
-  }
-  return cached as Post[];
+  const req = await fetch("/data.json");
+  return await req.json();
+}
+
+const cached = getDataInner();
+export async function getData(): Promise<Post[]> {
+  return await cached;
 }
