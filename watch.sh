108fef307c00acba1348cb5136197cbf5d7514cb
Liu Xiaoyi <circuitcoder0@gmail.com>
2025-05-13 00:19:01 +0800

    Added logging into watch.sh

--- watch.sh
+++ watch.sh
@@ -3,4 +3,4 @@
 set -e
 
 cd gen
-cargo run -- -p ../content -t ../fonts/SourceHanSerifSC-VF.otf -o ../web/src/assets/data.json --wght 900 --feed ../web/public/feed.xml --feed-cfg ./feed-cfg.json --subset-font ../web/src/assets/subset.woff2 --watch
+RUST_LOG=info cargo run -- -p ../content -t ../fonts/SourceHanSerifSC-VF.otf -o ../web/src/assets/data.json --wght 900 --feed ../web/public/feed.xml --feed-cfg ./feed-cfg.json --subset-font ../web/src/assets/subset.woff2 --watch
